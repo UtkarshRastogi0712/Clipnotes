117efb99dc2021820974c9960838cea9d4ac5690
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2023-10-20 14:49:01 +0530

    Card addes

---
+++ components/card.jsx
@@ -0,0 +1,8 @@
+import "./card.css";
+import React from "react";
+
+function Card() {
+  return <div className="card">Card</div>;
+}
+
+export default Card;
--- components/nonsidebar.jsx
+++ components/nonsidebar.jsx
@@ -1,14 +1,20 @@
 import "./nonsidebar.css";
 import React from "react";
 import Topbar from "./topbar";
+import Card from "./card";
 
 function NonSideBar() {
   return (
     <div className="non-side-bar">
       <Topbar />
       <div className="canvas">
-        <div className="note-title">Notes</div>
-        <div className="note-cards">Cards</div>
+        <div className="note-title">
+          <h1>Notes</h1>
+        </div>
+        <dev className="cards">
+          {Array(4).fill(<Card />)}
+          <Card />
+        </dev>
       </div>
     </div>
   );
